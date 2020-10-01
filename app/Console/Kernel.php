231f7b9de0e1640efcbac63897be4452c268e648
sosa6 <carlos.sosa@porta.com.py>
2020-10-01 15:57:14 -0400

    Sitemap

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\GenerarSitemap;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +26,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        // $schedule->job(new GenerarSitemap())->daily()->at('00:30');
+
+        $schedule->job(new GenerarSitemap())->everyMinute();
     }
 
     /**
